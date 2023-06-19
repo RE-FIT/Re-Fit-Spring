29e90a3d6e272c6114ce1f3649eb2ae0e5b75a51
Ji Seungmin <cswcsm02@gmail.com>
2023-06-20 00:50:55 +0900

    init

---
+++ src/main/java/com/umc/refit/RefitApplication.java
@@ -0,0 +1,13 @@
+package com.umc.refit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefitApplication.class, args);
+	}
+
+}
