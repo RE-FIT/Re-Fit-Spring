0d8dae1c4911f4af638cb90146972411c979f85c
Ji Seungmin <cswcsm02@gmail.com>
2023-06-21 15:07:53 +0900

    [test]: api test Controller 생성

---
+++ src/main/java/com/umc/refit/web/controller/ApiController.java
@@ -0,0 +1,19 @@
+package com.umc.refit.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/{name}")
+    public String name(@PathVariable("name") String name) {
+        return name;
+    }
+}
