2fe94c937bfc2de0ef75df6dd9ed938ec12cebe1
Ji Seungmin <cswcsm02@gmail.com>
2023-06-28 01:04:55 +0900

    [refactor]: email api return dto 수정

--- src/main/java/com/umc/refit/web/controller/MemberController.java
+++ src/main/java/com/umc/refit/web/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.umc.refit.web.controller;
 
 import com.umc.refit.domain.dto.member.EmailDto;
+import com.umc.refit.domain.dto.member.EmailResDto;
 import com.umc.refit.web.service.EmailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +20,10 @@ public class MemberController {
 
     /*이메일 인증 API*/
     @PostMapping("/email")
-    public String email(@RequestBody EmailDto emailDto) throws MessagingException {
+    public EmailResDto email(@RequestBody EmailDto emailDto) throws MessagingException {
 
         String auth = emailService.sendEmail(emailDto.getEmail());
 
-        return auth;
+        return new EmailResDto(auth);
     }
 }
