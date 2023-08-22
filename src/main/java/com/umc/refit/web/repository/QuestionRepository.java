bceadbeaec58fc92dea2050f69b7d2ea3ee7083f
choidongkuen <danaver12@daum.net>
2023-08-22 15:30:26 +0900

    [refactor] : 숲 메인 화면 조회 API 리팩토링

--- src/main/java/com/umc/refit/web/repository/QuestionRepository.java
+++ src/main/java/com/umc/refit/web/repository/QuestionRepository.java
@@ -2,7 +2,12 @@ package com.umc.refit.web.repository;
 
 import com.umc.refit.domain.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
 
+    @Query(value = "SELECT * FROM question ORDER BY RAND() LIMIT :count", nativeQuery = true)
+    List<Question> findRandomQuestions(int count);
 }
