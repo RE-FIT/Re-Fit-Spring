6d2144494782e97b2ef58d9029eed2923c8f6c0a
Ji Seungmin <cswcsm02@gmail.com>
2023-12-18 18:31:42 +0900

    refactor: CustomAuthenticationFailureHandler에 Early Return 패턴 적용하여 리팩토링

--- src/main/java/com/umc/refit/web/filter/exception/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/umc/refit/web/filter/exception/CustomAuthenticationFailureHandler.java
@@ -14,8 +14,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.HashMap;
 
-import static com.umc.refit.exception.ExceptionType.LOGIN_FAILED;
-import static com.umc.refit.exception.ExceptionType.LOGIN_FAILED_ALL;
+import static com.umc.refit.exception.ExceptionType.*;
 
 @Component
 public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
@@ -23,23 +22,22 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
+        if (exception instanceof UsernameNotFoundException || exception instanceof BadCredentialsException) {
+            sendErrorResponse(response, LOGIN_FAILED.getErrorMessage(), LOGIN_FAILED.getCode());
+            return;
+        }
 
+        if (exception instanceof LoginException) {
+            sendErrorResponse(response, ((LoginException) exception).getErrorMessage(), ((LoginException) exception).getCode());
+            return;
+        }
+
+        sendErrorResponse(response, LOGIN_FAILED_UNKNOWN.getErrorMessage(), LOGIN_FAILED_UNKNOWN.getCode());
+    }
+
+    private void sendErrorResponse(HttpServletResponse response, String errorMessage, int code) throws IOException {
         response.setStatus(HttpStatus.BAD_REQUEST.value());
         response.setContentType("application/json;charset=UTF-8");
-
-        String errorMessage;
-        int code;
-
-        if ((exception instanceof UsernameNotFoundException) || (exception instanceof BadCredentialsException)){
-            errorMessage = LOGIN_FAILED.getErrorMessage();
-            code = LOGIN_FAILED.getCode();
-        } else if (exception instanceof LoginException) {
-            errorMessage = ((LoginException) exception).getErrorMessage();
-            code = ((LoginException) exception).getCode();
-        } else {
-            errorMessage = LOGIN_FAILED_ALL.getErrorMessage();
-            code = LOGIN_FAILED_ALL.getCode();
-        }
 
         ObjectMapper mapper = new ObjectMapper();
         HashMap<String, Object> errorResponse = new HashMap<>();
